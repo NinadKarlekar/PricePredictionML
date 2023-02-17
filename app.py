66113cbc663e5d7ba4ed45942376d58d3af68d89
Ninad Karlekar <ninad.karlekar@vsit.edu.in>
2023-02-17 15:46:45 +0530

    scaling.pkl added

--- app.py
+++ app.py
@@ -5,4 +5,14 @@ import pandas as pd
 
 app=Flask(__name__)
 # Load the model
-model = pickle.load(open('regmodel.pkl','rb'))
+regmodel = pickle.load(open('regmodel.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+
+def predict_api():
+    data = request.json['data']
+    print(data)
