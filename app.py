5d2685517023f61f8c9ea30f04e68305f7081f63
Ninad Karlekar <ninad.karlekar@vsit.edu.in>
2023-02-17 17:23:30 +0530

    html page created

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict_api',methods=['POST'])
-
 def predict_api():
     data = request.json['data']
     print(data)
@@ -22,6 +21,14 @@ def predict_api():
     output = regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text = "The house price is {}".format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
